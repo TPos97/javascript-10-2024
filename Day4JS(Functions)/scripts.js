d186604f0e845752a2e51cd7a03acc9947b6b63c
Tpos97 <tryortpos97@gmail.com>
2024-10-24 11:02:30 -0400

    .

---
+++ Day4JS(Functions)/scripts.js
@@ -0,0 +1,14 @@
+//alert("Testing JS")
+
+
+document.getElementById("date").innerHTML = Date()
+
+function MyGreetingMsg(){
+    document.getElementById("welcome").innerHTML = "Welcome to learning Javascript"
+}
+function Return(){
+    document.getElementById("welcome").innerHTML = "Welcome to CBC"
+}
+function Datebutton2(){
+    document.getElementById("date2").innerHTML = Date()
+}
