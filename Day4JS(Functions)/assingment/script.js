e67563c378cdba54a4b73be956170f6344756bfc
Tpos97 <tryortpos97@gmail.com>
2024-10-25 09:33:56 -0400

    .

---
+++ Day4JS(Functions)/assingment/script.js
@@ -0,0 +1,6 @@
+function authors(){
+    document.getElementById("stories").innerHTML = "My Favorite Authors are George R.R. Martin and Edgar Allen Poe"
+}
+window.onload = function(){
+    alert("Welcome my favorite authors Page");
+};
