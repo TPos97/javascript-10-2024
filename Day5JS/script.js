17960109d690a3e22cb3ccd6672c6152470fb1e9
Tpos97 <tryortpos97@gmail.com>
2024-10-25 11:14:34 -0400

    .

---
+++ Day5JS/script.js
@@ -0,0 +1,30 @@
+function hbtn1(){
+    document.getElementById('header').style.color='blue'
+}
+function hbtn2(){
+    document.getElementById('header').style.color='black'
+}
+function blubon(){
+    document.getElementById('myImage').src = '../Day5JS/images/bulb.png';
+}
+function bulboff(){
+    document.getElementById('myImage').src = '../Day5JS/images/bulboff.png';
+}
+function increasefont(){
+    document.getElementById('para').style.fontSize="32px";
+}
+function decreasefont(){
+    document.getElementById('para').style.fontSize="16px";
+}
+function btnred(){
+    document.getElementById('header-2').style.color='red';
+}
+function btnyellow(){
+    document.getElementById('header-2').style.color='yellow';
+}
+function btngreen(){
+    document.getElementById('header-2').style.color='green';
+}
+function btnblue(){
+    document.getElementById('header-2').style.color='blue';
+}
