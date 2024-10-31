dd9bc710f89b149cd48a52f5bd8172820971f0e4
Tpos97 <tryortpos97@gmail.com>
2024-10-31 13:31:04 -0400

    .

--- Projects/tipcalc/script.js
+++
@@ -1,8 +0,0 @@
-function calculatetip(){
-    var mybillAmount = document.getElementById("bill").value;
-    var myTip = document.getElementById("tip").value;
-    
-    var totalAmount = mybillAmount * (1+myTip/100);
-
-    document.getElementById('total').innerHTML = totalAmount.toFixed()
-}
